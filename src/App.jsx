34b841af5ec66ff26c7dd15e2b569e10cbd2d0e2
TomasSandoval <tomassandoval079@gmail.com>
2023-06-08 00:28:13 -0300

    bot fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,20 @@ import Home from './components/home/home'
 import Nav from './components/Nav/Nav'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import ChatBot from 'react-simple-chatbot';
+import Avatar from './assets/Avatar chat.png';
+import { useState } from 'react'
+import Bot from './components/ChatBot/ChatBot';
 
 
 
 
 
 function App() {
+
   return (
     <>
     <Nav/>
-    <ChatBot
-    steps={[
-      {
-        id: '1',
-        message: 'What number I am thinking?',
-        trigger: '2',
-      },
-      {
-        id: '2',
-        options: [
-          { value: 1, label: 'Number 1', trigger: '4' },
-          { value: 2, label: 'Number 2', trigger: '3' },
-          { value: 3, label: 'Number 3', trigger: '3' },
-        ],
-      },
-      {
-        id: '3',
-        message: 'Wrong answer, try again.',
-        trigger: '2',
-      },
-      {
-        id: '4',
-        message: 'Awesome! You are a telepath!',
-        end: true,
-      },
-    ]}
-  />
+    <Bot/>
     <Routes>
       <Route path='/' element= {<Home></Home>}/>
       <Route path='/about'/>
---
+++ src/components/ChatBot/ChatBot.jsx
@@ -0,0 +1,77 @@
+import "./ChatBot.css";
+import ChatBot from 'react-simple-chatbot';
+import { useState } from 'react';
+import Avatar from '../../assets/Avatar chat.png';
+
+
+function Bot() {
+    const [showChat, setShowChat] = useState(false)
+
+
+    const handleShow = () => {
+        setShowChat(!showChat)
+      }
+
+
+
+    return (
+        <div className="chatbot-container">
+            {showChat && <ChatBot
+                headerTitle="ChatBot"
+                steps={[
+                    {
+                        id: '1',
+                        message: 'Hello, how can I help you?',
+                        trigger: '2',
+                    },
+                    {
+                        id: '2',
+                        options: [
+                            { value: 1, label: 'About me', trigger: '3' },
+                            { value: 2, label: 'Portfolio', trigger: '4' },
+                            { value: 3, label: 'Contact', trigger: '5' },
+                        ],
+                    },
+                    {
+                        id: '3',
+                        message: 'I am a full stack developer, I have knowledge in HTML, CSS, JavaScript, React, Redux, Node, Express, PostgreSQL, Sequelize, among others.',
+                        trigger: '6',
+                    },
+                    {
+                        id: '4',
+                        message: 'Check out my work, from basic landing pages to professional e-commerce sites built with market-leading technologies. I perform well working independently, but what I enjoy most is working with a team that shares the same passion for programming!',
+                        trigger: '6',
+                    },
+                    {
+                        id: '5',
+                        message: 'You can contact me through my email: tomassandoval079@gmail.com',
+                        trigger: '6',
+                    },
+
+                    {
+                        id: '6',
+                        message: 'Do you want to know something else?',
+                        trigger: '7',
+                    },
+                    {
+                        id: '7',
+                        options: [
+                            { value: 1, label: 'Yes', trigger: '2' },
+                            { value: 2, label: 'No', trigger: '8' },
+                        ],
+                    },
+                    {
+                        id: '8',
+                        message: 'Thank you for visiting my website!',
+                        end: true,
+                    },
+                ]}
+            />}
+            <img onClick={handleShow} className="avatar-chat" src={Avatar} alt="Avatar logo"></img>
+        </div>
+    )
+}
+
+
+
+export default Bot;
