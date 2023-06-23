30b0cdbd8d3121ffdbfb37fb13a9944c759c5fb0
TomasSandoval <tomassandoval079@gmail.com>
2023-06-23 17:28:06 -0300

    change language chatbot

--- src/components/ChatBot/ChatBot.jsx
+++ src/components/ChatBot/ChatBot.jsx
@@ -4,10 +4,13 @@ import { useState } from "react";
 import Avatar from "../../assets/Avatar chat.png";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 function Bot() {
   const [showChat, setShowChat] = useState(false);
   const navigate = useNavigate();
+  const [t, i18n] = useTranslation("global");
+
 
   const handleShow = () => {
     setShowChat(!showChat);
@@ -46,7 +49,7 @@ function Bot() {
 
   return (
     <div className="chatbot-container">
-      {showChat && (
+      {showChat && i18n.language === "en" && (
         <ChatBot
         cache={false}
           botAvatar={Avatar}
@@ -120,6 +123,82 @@ function Bot() {
           ]}
         />
       )}
+      {
+        showChat && i18n.language === "es" && (
+          <ChatBot
+        cache={false}
+          botAvatar={Avatar}
+          headerTitle="ChatBot"
+          handleEnd={handleEnd}
+          steps={[
+            {
+              id: "1",
+              message: "Hola, ¿como puedo ayudarte?",
+              trigger: "2",
+            },
+            {
+              id: "2",
+              options: [
+                { value: 1, label: "Me gustaria conocerte mejor.", trigger: "3" },
+                { value: 4, label: "Me gustaria ver tus projectos.", trigger: "8" },
+                {
+                  value: 5,
+                  label: "Me gustaria contactarte.",
+                  trigger: "9",
+                },
+                {
+                  value: 6,
+                  label: "Quisiera ver tu curriculum.",
+                  trigger: "10",
+                }
+              ]
+            },
+            {
+              id: "3",
+              message: "Mi nombre es Tomás Sandoval y soy un desarrollador full stack apasionado por el desarrollo de software!",
+              trigger: "4",
+            },
+            {
+              id: "4",
+              message: "¿Quieres saber más sobre mí?",
+              trigger: "5",
+            },
+            {
+              id: "5",
+              options: [
+                { value: 2, label: "Si", trigger: "6" },
+                { value: 3, label: "No", trigger: "7" },
+              ]
+            },
+            {
+              id: "6",
+              message: "Vamos a la sección 'sobre mí'!",
+              end: true,
+            },
+            {
+              id: "7",
+              message: "Okay, ¿en que otra cosa puedo ayudarte?",
+              trigger: "2"
+            },
+            {
+              id: "8",
+              message: "Vamos a la sección 'Portfolio'!",
+              end: true,
+            },
+            {
+              id: "9",
+              message: "Vamos a la sección de contacto!",
+              end: true,
+            },
+            {
+              id: "10",
+              message: "Por supuesto, estoy descargando mi curriculum en tu dispositivo ahora mismo!",
+              end: true,
+            }
+          ]}
+        />
+        )
+      }
       <img
         onClick={handleShow}
         className="avatar-chat"
